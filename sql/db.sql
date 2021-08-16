64cdb01a9c4ce4460e65ed8812f0082c5a05c7d7
deadfantom2 <cameratest81@gmail.com>
2021-08-16 10:19:33 +0200

    'Add_in_sql_insert_row'

--- sql/db.sql
+++ sql/db.sql
@@ -54,10 +54,21 @@ SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
+        INSERT INTO users (name, email, password) VALUES ('frfrfr', 'frfr@frfr.frfr', '$2b$10$xtMX5ORGsnUDz82hKKqknuW97K7W.H6JxR7c3ezgrrS1nDNgd7Y4W');
+        INSERT INTO users (name, email, password) VALUES ('dedede', 'dede@dede.dede', '$2b$10$WTOditCTgSuJcffrbId/e.7nZcj2XR.VNnk56FwliaEWFTvjN3FzW');
 
 
+        INSERT INTO tickets (user_id, titre, description, status) VALUES (1,'Ticket - 1','Ticket: display a list of users','todo');
+        INSERT INTO tickets (user_id, titre, description, status) VALUES (1,'Ticket - 2','Ticket: delete one user from table','done');
+        INSERT INTO tickets (user_id, titre, description, status) VALUES (2,'Ticket - 1','Ticket: edit two users from table','wip');
 
+        INSERT INTO comments (user_id, ticket_id, description) VALUES (1, 1, 'Comment for list of users - add please one user in table');
+        INSERT INTO comments (user_id, ticket_id, description) VALUES (1, 1, 'Comment for list of users - delete please two users from table');
+        INSERT INTO comments (user_id, ticket_id, description) VALUES (1, 1, 'Comment for list of users - edit please one user from table')
 
+        INSERT INTO comments (user_id, ticket_id, description) VALUES (1, 2, 'Comment for list of users - add please one user in table')
+        INSERT INTO comments (user_id, ticket_id, description) VALUES (1, 2, 'Comment for list of users - delete please two users from table')
+        INSERT INTO comments (user_id, ticket_id, description) VALUES (1, 2, 'Comment for list of users - edit please one user from table')
 
 
 
